8635171e9c53f3d3cf9eafb3c7e5a2e3ab723352
Wes Waters <weswaters@woosterbrush.com>
2023-02-24 13:04:54 -0500

    adjusted date display

--- public/main.js
+++ public/main.js
@@ -168,11 +168,12 @@ createTeamButton.addEventListener('click', (e)=>{
 })
 
 function formatDate(date) {
-  console.log('Input Date' + date)
-  var d = new Date(date),
-    month = '' + (d.getMonth() +1),
-    day = '' + d.getDate(),
-    year = '' + d.getFullYear();
+  var d = new Date(date);
+  d.setHours(10)
+  month = '' + (d.getMonth() +1),
+  day = '' + (d.getDate() +1),
+  year = '' + d.getFullYear();
+
 
     if(month.length < 2){
       month = '0' + month;
